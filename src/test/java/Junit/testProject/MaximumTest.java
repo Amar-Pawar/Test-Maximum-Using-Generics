62491126160996c46bf33d9bd16a47485a10712f
Amar-Pawar <pawaramar.pawar@gmail.com>
2021-05-07 00:46:07 +0530

    add:refactored the code to add all three methods in one generic method

--- src/test/java/Junit/testProject/MaximumTest.java
+++ src/test/java/Junit/testProject/MaximumTest.java
@@ -8,24 +8,68 @@ import org.junit.Test;
 
 public class MaximumTest {
 	
+	
+	@Test
+	public void shouldReturnTrue_WhenMaxIntegerAt1stPosition ()
+	{
+		 int max=Maximum.printMax(5,1,2);
+		 assertEquals( 5 , max );
+	}
+	    
+	@Test
+	public void shouldReturnTrue_WhenMaxIntegerAt2ndPosition()
+	{
+		 int max=Maximum.printMax(1,5,2);
+	     assertEquals( 5 , max );
+	}
+	    
+	@Test
+	public void shouldReturnTrue_WhenMaxIntegerAt3rdPosition()
+	{
+	    int max=Maximum.printMax(1,2,5);
+	    assertEquals( 5 , max );
+	}
+	
+
+	@Test
+	public void shouldReturnTrue_WhenMaxDoubleAt1stPosition ()
+	{
+	    double max=Maximum.printMax(5.1,1.3,2.8);
+	    assertEquals( 5.1 , max , 0);
+	}
+
+	@Test
+	public void shouldReturnTrue_WhenMaxDoubleAt2ndPosition()
+	{
+		double max=Maximum.printMax(1.3,5.1,2.8);
+		assertEquals( 5.1 , max , 0 );
+	}
+    
+	@Test
+	public void shouldReturnTrue_WhenMaxDoubleAt3rdPosition()
+	{
+		double max=Maximum.printMax(1.3,2.8,5.1);
+		assertEquals( 5.1 , max , 0 );
+	}
+	
 	@Test
     public void shouldReturnTrue_WhenMaxStringAt1stPosition ()
     {
-    	String max=Maximum.maxString("good", "better", "bad");
+    	String max=Maximum.printMax("good", "better", "bad");
         assertEquals( "good" , max );
     }
     
     @Test
     public void shouldReturnTrue_WhenMaxStringAt2ndPosition()
     {
-    	String max=Maximum.maxString("bad", "good", "better");
+    	String max=Maximum.printMax("bad", "good", "better");
         assertEquals( "good" , max );
     }
     
 	@Test
     public void shouldReturnTrue_WhenMaxStringAt3rdPosition()
     {
-    	String max=Maximum.maxString("bad", "better", "good");
+    	String max=Maximum.printMax("bad", "better", "good");
         assertEquals( "good" , max );
     }
 }
